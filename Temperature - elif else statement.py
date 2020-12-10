3e0982bcc06f1c92c63a8705e9ec89e32e64d176
cvanfossen368 <47308215+cvanfossen368@users.noreply.github.com>
2020-12-10 10:17:31 -0500

    Delete Temperature - elif else statement.py

--- Temperature - elif else statement.py
+++
@@ -1,9 +0,0 @@
-temperature = 71
-if temperature > 30:
-    print("It's warm")
-    print("Drink water")
-elif temperature > 60:
-    print("It's nice")
-else:
-    print("It's cold")
-print("Done")
